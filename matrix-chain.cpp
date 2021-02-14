c007d6e08466b5964aa4cf4488df09e29fbbd87e
archeryue7 <archeryue7@gmail.com>
2021-02-14 20:31:48 +0800

    MOD: refine code

--- matrix-chain.cpp
+++ matrix-chain.cpp
@@ -4,39 +4,38 @@
 using namespace std;
 
 int main(int argv, char **args) {
+    // input
     int n;
     cin >> n;
     int x, y;
     vector<pair<int, int>> matrix_chain;
-    vector<vector<int>> cal_table(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         cin >> x >> y;
         matrix_chain.push_back(make_pair(x, y));
     }
+    // init
+    vector<vector<int>> cal_table(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         cal_table[i][i] = 0;
     }
+    // dp
     for (int i = 1; i < n; i++) {
-        // cal_table[i - 1][i] = matrix_chain[i - 1].first *
-        // matrix_chain[i].first * matrix_chain[i].second;
         for (int j = i - 1; j >= 0; j--) {
             int min = INT_MAX;
             int base = matrix_chain[j].first * matrix_chain[i].second;
             for (int k = j; k < i; k++) {
                 int res = cal_table[j][k] + cal_table[k + 1][i] +
                           (base * matrix_chain[k].second);
-                if (res < min) {
-                    min = res;
-                }
+                min = res < min ? res : min;
             }
             cal_table[j][i] = min;
         }
     }
-
+    // output
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (j < i) {
-                cout << "  ";
+                cout << "0 ";
             } else {
                 cout << cal_table[i][j] << " ";
             }
