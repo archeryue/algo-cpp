09ff2a9078e2228dde1286d5e58192834b29a9e7
archeryue <archeryue7@gmail.com>
2021-06-22 17:37:46 +0800

    FIX: int flow

--- leetcode7.cpp
+++ leetcode7.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int reverse(int x) {
 	x = abs(x);
 	int ret = 0;
 	while (x > 0) {
+		if (ret > INT_MAX / 10) return 0;
 		ret *= 10;
 		ret += x % 10;
 		x /= 10;
