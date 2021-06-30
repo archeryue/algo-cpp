878474294b37659180ef12e68462b1e06ba635b9
archeryue7 <archeryue7@gmail.com>
2021-06-30 21:55:50 +0800

    MOD: base64 code style

--- base64.cpp
+++ base64.cpp
@@ -12,13 +12,14 @@ vector<char> kTable {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
 string Encode(vector<char> bytes) {
-	int groupSize = bytes.size() / 3;
 	string ret;
+	int groupSize = bytes.size() / 3;
 	uint32_t buf = 0;
 	uint32_t mark = 0xfc000000;
+	// 24 bits for a group
 	for (int i = 0; i < groupSize; i++) {
 		for (int j = 0; j < 3; j++) {
-			buf = buf | bytes[3*i + j];
+			buf = buf | bytes[3 * i + j];
 			buf = buf << 8;
 		}
 		for (int j = 0; j < 4; j++) {
@@ -26,24 +27,25 @@ string Encode(vector<char> bytes) {
 			ret.push_back(kTable[index]);
 			buf = buf << 6;
 		}
-		buf = 0;
 	}
+
 	int leftCount = bytes.size() % 3; 
-	if (leftCount == 0) return ret;
-	// padding
-	for (int i = 0; i < 3; i++) {
-		if (i < leftCount) {
-			buf = buf | bytes[groupSize * 3 + i];
+	if (leftCount > 0) {
+		// handle padding, left 1 or 2
+		for (int i = 0; i < 3; i++) {
+			if (i < leftCount) {
+				buf = buf | bytes[groupSize * 3 + i];
+			}
+			buf = buf << 8;
 		}
-		buf = buf << 8;
-	}
-	for (int i = 0; i < leftCount + 1; i++) {
-		uint32_t index = (buf & mark) >> 26;
-		ret.push_back(kTable[index]);
-		buf = buf << 6;
-	}
-	for (int i = 0; i < (3-leftCount); i++) {
-		ret.push_back('=');
+		for (int i = 0; i < leftCount + 1; i++) {
+			uint32_t index = (buf & mark) >> 26;
+			ret.push_back(kTable[index]);
+			buf = buf << 6;
+		}
+		for (int i = 0; i < (3 - leftCount); i++) {
+			ret.push_back('=');
+		}
 	}
 	return ret;
 }
