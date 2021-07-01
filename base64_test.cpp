1320290c9248147ffdfaa0855025c2e416589c50
archeryue7 <archeryue7@gmail.com>
2021-07-01 14:36:00 +0800

    MOD: make base64 a single h file

---
+++ base64_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "base64.h"
+
+using namespace std;
+
+int main() {
+	vector<uint8_t> bytes {'A', 'B', 'C'};
+	cout << Encode(bytes) << endl;
+	for (auto byte : Decode(Encode(bytes))) {
+		cout << (char)byte << " ";
+	}
+	cout << endl;
+	bytes = {'s', 'd', 'f', '1', '2'};
+	cout << Encode(bytes) << endl;
+	for (auto byte : Decode(Encode(bytes))) {
+		cout << (char)byte << " ";
+	}
+	cout << endl;
+	return 0;
+}
