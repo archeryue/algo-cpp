6b9512530be2eef6167cd85ac2dee4fff3b46492
archeryue7 <archeryue7@gmail.com>
2021-07-01 12:42:26 +0800

    ADD: base64 decode

--- base64.cpp
+++ base64.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include <string>
 
@@ -11,7 +12,17 @@ vector<char> kTable {
 		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
-string Encode(vector<char> bytes) {
+uint8_t getIndex(char ch) {
+	if (ch >= 'A' && ch <= 'Z') return ch - 'A';
+	else if (ch >= 'a' && ch <= 'z') return 26 + ch - 'a';
+	else if (ch >= '0' && ch <= '9') return 52 + ch - '0';
+	else if (ch == '+') return 62;
+	else if (ch == '/') return 63;
+	else if (ch == '=') return 0; // padding
+	else throw runtime_error("invalid base64 char: " + to_string(ch));
+}
+
+string Encode(vector<uint8_t> bytes) {
 	string ret;
 	// 24 bits for a group
 	int groupSize = bytes.size() / 3;
@@ -37,10 +48,44 @@ string Encode(vector<char> bytes) {
 	return ret;
 }
 
+vector<uint8_t> Decode(string str) {
+	vector<uint8_t> ret;
+	// 24 bits for a group
+	int groupSize = str.length() / 4;
+	// get unpadding length
+	int unpaddingLen = str.length();
+	while (str[unpaddingLen - 1] == '=') unpaddingLen--;
+	// decode
+	uint32_t buf = 0;
+	for (int i = 0; i < groupSize; i++) {
+		for (int j = 0; j < 4; j++) {
+			buf = buf | getIndex(str[4 * i + j]);
+			buf = buf << 6;
+		}
+		// left align
+		buf = buf << 2;
+		for (int j = 0; j < 3; j++) {
+			if ((3 * i + j) * 8 < unpaddingLen * 6) {
+				ret.push_back(0xff & (buf >> 24));
+			}
+			buf = buf << 8;
+		}
+	}
+	return ret;
+}
+
 int main() {
-	vector<char> bytes {'A', 'B', 'C'};
+	vector<uint8_t> bytes {'A', 'B', 'C'};
 	cout << Encode(bytes) << endl;
+	for (auto byte : Decode(Encode(bytes))) {
+		cout << (char)byte << " ";
+	}
+	cout << endl;
 	bytes = {'s', 'd', 'f', '1', '2'};
 	cout << Encode(bytes) << endl;
+	for (auto byte : Decode(Encode(bytes))) {
+		cout << (char)byte << " ";
+	}
+	cout << endl;
 	return 0;
 }
