9cca3ebf0cabb5f5b0a2a883879412fa8d533aae
archeryue <archeryue7@gmail.com>
2024-04-20 17:36:32 +0800

    MOD: output format for base64_test

--- base64_test.cpp
+++ base64_test.cpp
@@ -8,13 +8,25 @@ using namespace std;
 
 int main() {
     vector<uint8_t> bytes {'A', 'B', 'C'};
-    cout << Encode(bytes) << endl;
+    cout << "testcase 1 " << endl;
+    cout << "Source : ";
+    for (auto byte : bytes) {
+        cout << (char)byte << " ";
+    }
+    cout << endl << "Encode : ";
+    cout << Encode(bytes) << endl << "Decode : ";
     for (auto byte : Decode(Encode(bytes))) {
         cout << (char)byte << " ";
     }
     cout << endl;
+    cout << "testcase 2 " << endl;
+    cout << "Source : ";
     bytes = {'s', 'd', 'f', '1', '2'};
-    cout << Encode(bytes) << endl;
+    for (auto byte : bytes) {
+        cout << (char)byte << " ";
+    }
+    cout << endl << "Encode : ";
+    cout << Encode(bytes) << endl << "Decode : ";
     for (auto byte : Decode(Encode(bytes))) {
         cout << (char)byte << " ";
     }
